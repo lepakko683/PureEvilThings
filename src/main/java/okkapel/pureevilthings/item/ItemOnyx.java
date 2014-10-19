5cf297c1a3ee2faa0622c96661827c092c8a3c54
OkkapeL <lepakko683@gmail.com>
2014-10-19 20:48:35 +0300

    Fixed Blood Onyx crafting

--- src/main/java/okkapel/pureevilthings/item/ItemOnyx.java
+++ src/main/java/okkapel/pureevilthings/item/ItemOnyx.java
@@ -2,9 +2,13 @@ package okkapel.pureevilthings.item;
 
 import java.util.List;
 
+import okkapel.pureevilthings.entity.EntityItemOnyx;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 public class ItemOnyx extends ItemET {
 
@@ -26,6 +30,9 @@ public class ItemOnyx extends ItemET {
 		case 1:
 			lst.add(StatCollector.translateToLocal("item.bloodStainedOnyx.desc"));
 			break;
+		case 2:
+			lst.add(StatCollector.translateToLocal("item.bloodOnyx.desc"));
+			break;
 		}
 	}
 	
@@ -40,6 +47,26 @@ public class ItemOnyx extends ItemET {
 			return "item.bloodOnyx";
 		}
 		return super.getUnlocalizedName(stack);
+	}
+	
+	@Override
+	public Entity createEntity(World world, Entity loc, ItemStack stack) {
+		if(stack.getItem() == this) {
+			EntityItemOnyx drop = new EntityItemOnyx(world, loc.posX, loc.posY, loc.posZ, stack);
+			drop.motionX = loc.motionX;
+			drop.motionY = loc.motionY;
+			drop.motionZ = loc.motionZ;
+			drop.delayBeforeCanPickup = 40;
+			return drop;
+		} else {
+			System.out.println("This shouldn't have happened!");
+		}
+		return super.createEntity(world, loc, stack);
+	}
+	
+	@Override
+	public boolean hasCustomEntity(ItemStack stack) {
+		return true;
 	}
 
 }
